25942523a041c2adb584232675fb657b6cbab551
aydek <kesa0915@hotmail.com>
2022-04-23 03:39:23 +0100

    Removed unused deps

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@ import ThemeProvider from './ThemeProvider';
 import { Navbar } from './components/Navbar';
 import './style/App.scss';
 import { Home } from './components/Home';
-import { Social } from './components/Social';
 import { About } from './components/About';
-import { MutableRefObject, useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import useIsMounted from './hooks/isMounted';
 
 let timeout = Date.now();
