8cbe2ad529804ac96f861cd32894fcb228913a24
aydek <kesa0915@hotmail.com>
2022-04-27 20:09:10 +0100

    updated typewriter animation

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import useIsMounted from '../hooks/isMounted';
 import '../style/Home.scss';
 import { ArrowDown } from './icons/ArrowDown';
 import { Github } from './icons/Github';
@@ -6,14 +8,31 @@ import { Sass } from './icons/Sass';
 import { Typescript } from './icons/Typescript';
 import { Vite } from './icons/Vite';
 
+const headText = 'Welcome to my website!';
+let headIndex = 0;
+
 export const Home = ({ setIndex, className }: { setIndex: React.Dispatch<React.SetStateAction<number>>; className: string }) => {
+    const [head, setHead] = useState('');
+
+    const isMounted = useIsMounted();
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (headIndex >= headText.length - 1) return;
+            console.log(head);
+            console.log(headIndex);
+            if (isMounted) headIndex = headIndex + 1;
+            setHead(head + headText[headIndex]);
+        }, 300);
+    }, [head]);
+
     const handleClick = () => {
         setIndex(1);
     };
 
     return (
         <div className={`home__body ${className}`}>
-            <div className="main__text">Welcome to my website!</div>
+            <div className="main__text">{head}</div>
             <div className="sub__text">My name is Kestutis and I am a developer...</div>
             <a href="https://github.com/aydek/my-portfolio" className="link__button">
                 <Github />
